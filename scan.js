753018708cd1c5f68e08caf50a04d365eb5a5f65
Gene Connolly <connolly.gene@gmail.com>
2019-07-05 22:27:37 -0700

    add auth, redis

--- scan.js
+++ scan.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 const crypto = require('crypto')
-let index = 7445//8573 //jzi = 7445, 8572, qnl
+//let index = 7445//8573 //jzi = 7445, 8572, qnl
 
 // https://codereview.stackexchange.com/a/16129
 function convertToNumberingScheme(number) {
@@ -16,22 +16,24 @@ function convertToNumberingScheme(number) {
     return letters;
 }
 
-const interval = setInterval(() => {
-    let password = convertToNumberingScheme(index++);
-    console.log(password)
-    let hash = crypto.createHash("sha1").update(password).digest("hex")
-    let hashPrefix = hash.substring(0, 5)
-    let hashSuffix = hash.substring(5).toUpperCase()
+function scan(cursor, callback) {
+    const interval = setInterval(() => {
+        let password = convertToNumberingScheme(cursor++);
+        let hash = crypto.createHash("sha1").update(password).digest("hex")
+        let hashPrefix = hash.substring(0, 5)
+        let hashSuffix = hash.substring(5).toUpperCase()
 
-    fetch(`https://api.pwnedpasswords.com/range/${hashPrefix}`)
-        .then(res => res.text())
-        .then(body => body.split('\r\n'))
-        .then(matches => matches.some(tuple => tuple.startsWith(hashSuffix)))
-        .then(ispwned => {
-            if(!ispwned) {
-                console.log(`SUCCESS ${password}`)
-                clearInterval(interval)
-            }
-        })
-}, 1500)
+        fetch(`https://api.pwnedpasswords.com/range/${hashPrefix}`)
+            .then(res => res.text())
+            .then(body => body.split('\r\n'))
+            .then(matches => matches.some(tuple => tuple.startsWith(hashSuffix)))
+            .then(ispwned => {
+                if(!ispwned) {
+                    clearInterval(interval)
+                    callback(null, { cursor, password })
+                }
+            })
+    }, 1500)
+}
 
+module.exports = scan
--- server.js
+++ server.js
@@ -1,18 +1,114 @@
+// express
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const twitterParse = require('twitter-url-parser')
-
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send('OK')
+  res.send('OK')
 })
 
+// redis
+const redis = require('redis')
+const client = redis.createClient(process.env.REDIS_URL)
+
+// twitter stuff
+const twitterParse = require('twitter-url-parser')
+const twitterAPI = require('node-twitter-api')
+const twitter = new twitterAPI({
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  callback: process.env.TWITTER_OAUTH_CALLBACK || 'http://localhost:8080/oauth'
+})
+
+// scan
+const scan = require('./scan')
+
 app.post('/', (req, res) => {
-    const twitterId = twitterParse(req.body.link).id
-    console.log(twitterId)
-    res.send('OK')
+  const twitterId = twitterParse(req.body.link).id
+  console.log(twitterId)
+  client.hgetall('cursor', (error, cursor) => {
+    if(error) {
+      console.log('Error connecting to redis')
+      return
+    }
+
+    if(!cursor) {
+      console.log('Error retrieving cursor')
+      return
+    }
+
+    scan(cursor.value || 1740, (error, result) => {
+      if(error) {
+        console.log('Error scanning for value: ' + error)
+        return
+      }
+
+      console.log(result.password)
+
+      client.hmset('cursor', 'value', result.cursor, (error) => {
+        if (error) {
+          console.log(error)
+        }
+      })
+    })
+  })
+
+  res.send('OK')
+})
+
+app.get('/authenticate', (req, res) => {
+  twitter.getRequestToken((error, requestToken, requestTokenSecret, results) => {
+    if (error) {
+      console.log('Error getting OAuth request token : ' + JSON.stringify(error))
+    } else {
+      client.del('request')
+      client.hmset('request', 'token', requestToken, 'tokenSecret', requestTokenSecret, (error, result) => {
+        if (error) {
+          console.log(error)
+        } else {
+          res.redirect(twitter.getAuthUrl(requestToken))
+        }
+      })
+    }
+  })
+})
+
+app.get('/oauth', (req, res) => {
+  client.hgetall('request', (error, request) => {
+    if (error) {
+      console.log(error)
+    } else if (request) {
+      twitter.getAccessToken(
+        request.token,
+        request.tokenSecret,
+        req.query.oauth_verifier,
+        (error, accessToken, accessTokenSecret, results) => {
+          if (error) {
+            console.log(error)
+          } else {
+            twitter.verifyCredentials(
+              accessToken,
+              accessTokenSecret,
+              {},
+              (error, data, response) => {
+                if (error) {
+                  console.log(error)
+                } else {
+                  client.del('access')
+                  client.hmset('access', 'token', accessToken, 'tokenSecret', accessTokenSecret, (error, result) => {
+                    if (error) {
+                      console.log(error)
+                    } else {
+                      res.send(data['screen_name'])
+                    }
+                  })
+                }
+              })
+          }
+        })
+    }
+  })
 })
 
 app.listen(process.env.PORT || 8080, () => console.log('listening ' + (process.env.PORT || 8080)))
